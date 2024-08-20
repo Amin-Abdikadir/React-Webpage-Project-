d1ec14f28bc1af38897fa82029daa6d0d4691306
Amin Abdikadir <amino@Amins-MacBook-Pro.local>
2024-08-20 14:18:25 +0100

    contact form component completed

--- app/DesignBooking/page.js
+++ app/DesignBooking/page.js
@@ -1,34 +1,11 @@
 import ContactForm from "@/src/components/ContactForm/ContactForm";
 import styles from "./page.module.css";
 
-
-
-
-    export default function Page() {
-          return (
-            <div className={styles.SupaContainer}>
-                <h1 className={styles.FirstBox}>Design Booking</h1>
-
-                <h4>Personal Information</h4>
-
-                <ContactForm />
-
-
-                <h4>Contact information</h4>
-
-                <form className={styles.FormBox}>
-                  <label className={styles.Labels}>
-                      Phone number:
-                      <input type= "number"/>
-                  </label>
-                  <label className={styles.Labels}>
-                      Email Address:
-                      <input type= "email"/>
-                  </label>
-                
-                </form> 
-                <button type="submit" className={styles.Button}>Request Design Consultation</button>
-
-            </div>
-          )
-        };
+export default function Page() {
+  return (
+    <div className={styles.SupaContainer}>
+      <h1 className={styles.FirstBox}>Design Booking</h1>
+      <ContactForm />
+    </div>
+  );
+}
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,28 +1,52 @@
-import styles from './ContactForm.module.css';
-
+import styles from "./ContactForm.module.css";
+// import { useState } from "react";
 
 
 export default function ContactForm() {
 
- return (
-    <form className={styles.FormBox}>
+    // const [fullName,setFullName ] = useState({
+        
+    // })
+
+  return (
+    <>
+      <h4>Personal Information</h4>
+
+      <form className={styles.FormBox}>
         <label className={styles.Labels}>
-            Full Name:
-            <input type= "text"/>
+          Full Name:
+          <input type="text" />
         </label>
         <label className={styles.Labels}>
-            Postcode:
-            <input type= "text"/>
+          Postcode:
+          <input type="text" />
         </label>
         <label className={styles.Labels}>
-            House/Flat number and street Name
-            <input type= "text"/>
+          House/Flat number and street Name
+          <input type="text" />
         </label>
         <label className={styles.Labels}>
-            City:
-            <input type= "text"/>
+          City:
+          <input type="text" />
         </label>
-  </form>
-   
+
+        <h4 className={styles.ContactText}>Contact information</h4>
+
+        <label className={styles.Labels}>
+          Phone number:
+          <input type="number" />
+        </label>
+        <label className={styles.Labels}>
+          Email Address:
+          <input type="email" />
+        </label>
+
+       
+
+      </form> 
+      <button type="submit" className={styles.Button}>
+          Request Design Consultation
+        </button>
+    </>
   );
-};
+}
