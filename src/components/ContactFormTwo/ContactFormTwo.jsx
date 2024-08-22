487df675d262069988f57545b8217a7362de688d
Amin Abdikadir <amino@Amins-MacBook-Pro.local>
2024-08-22 13:46:27 +0100

    useReducer

--- src/components/ContactFormTwo/ContactFormTwo.jsx
+++ src/components/ContactFormTwo/ContactFormTwo.jsx
@@ -1,41 +1,103 @@
 "use client";
 
-import { useState } from "react";
+import { useReducer } from "react";
 
-import styles from "./styles.module.css"; 
+import styles from "./styles.module.css";
 
 const initialState = {
-    data: {
-        fullName: '',
-        
-    },
-    error: false,
+  data: {
+    fullName: "",
+    postcode: "", 
+  },
+  status: "editing",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CHANGE_FIELD":
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          [action.payload.fieldName]: action.payload.fieldValue,
+        },
+      };
+    case "ERROR":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "FORM_SUBMITTING":
+      return {
+        ...state,
+        status: "submitting",
+      };
+    case "FORM_SUCCESS":
+      return {
+        ...state,
+        status: "success",
+      };
+    default:
+      return state;
+  }
 }
 
-export default function ContactForm() {
-  const [fullName, setFullName] = useState("");
+export default function ContactFormOne() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const [error, setError] = useState(false);
+  console.log(state);
 
   function handleChange(event) {
-    if (event.target.name === "fullName") {
-      setFullName(event.target.value);
-    }
+    dispatch({
+      type: "CHANGE_FIELD",
+      payload: {
+        fieldName: event.target.name,
+        fieldValue: event.target.value,
+      },
+    });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    if (!fullName|) {
-      setError(true);| !email
-      return;
-    }
+    dispatch({
+      type: "FORM_SUBMITTING",
+    });
 
-    if (error) {
-      setError(false);
-    }
+    setTimeout(() => {
+      // if - any field is empty error ✅
 
-    console.log();
+      // if phone number is not a number error
+
+      // if email is not an email error
+
+      if (!state.data.fullName || !state.data.postcode) {
+        dispatch({
+          type: "ERROR",
+        });
+        return;
+      }
+
+      // if (/*not a number phoneNumber is a number not a string */) {
+      //     dispatch({
+      //         type: "ERROR"
+      //     });
+      //     return;
+      // }
+
+      // if (/*not a postcode*/) {
+      //     dispatch({
+      //         type: "ERROR",
+      //     });
+      //     return;
+      // }
+
+      dispatch({
+        type: "FORM_SUCCESS",
+      });
+
+      console.log("Data!!!!!!");
+    }, 5000);
   }
 
   return (
@@ -49,17 +111,34 @@ export default function ContactForm() {
             type="text"
             name="fullName"
             onChange={(event) => handleChange(event)}
-            value={fullName}
+            value={state.data.fullName}
+            className={styles.input}
+          />
+        </label>
+
+        <label className={styles.inputGroup}>
+          Postcode:
+          <input
+            type="text"
+            name="postcode"
+            onChange={(event) => handleChange(event)}
+            value={state.data.postcode}
             className={styles.input}
           />
         </label>
       </fieldset>
 
-      {error && <p className={styles.error}>Error</p>}
+      <fieldset className={styles.group}>
+        <legend className={styles.groupTitle}>Contact Info:</legend>
+      </fieldset>
+
+      {state.status === "error" && <p className={styles.error}>Error</p>}
 
       <button type="submit" className={styles.button}>
         Request Design Consultation
       </button>
+
+      {state.status}
     </form>
   );
 }
--- src/components/Hero/HeroDescription/HeroDescription.jsx
+++ src/components/Hero/HeroDescription/HeroDescription.jsx
@@ -1,16 +1,17 @@
 
-import styles from './HeroDescription.module.css'
+import styles from './HeroDescription.module.css' 
+import Link from "next/link";
 
 export default function HeroDescription() {
     return (
         <div className={styles.heroDescription}>
         <p className={styles.heroHeadline}>
           Discover the <br />
-          perfect fireplace ...
-        </p>
-        <p className={styles.consultationParagraph}>
-          Book consultation: <span className={styles.numberSpan}>0121 345 6789</span>
-        </p>
+          perfect fireplace ... 
+          </p> 
+          <button className={styles.ConsultationButton}> <Link href="/DesignBooking">
+    Book a consultation </Link>
+</button> 
       </div>
     )
 }
